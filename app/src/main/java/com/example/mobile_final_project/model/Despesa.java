09db44a3b381e6e2f4db3dd3973ad03c9d2bc840
Lucas Tito <lucassga500@gmail.com>
2022-09-17 19:31:34 -0300

    Created despesa class

---
+++ app/src/main/java/com/example/mobile_final_project/model/Despesa.java
@@ -0,0 +1,51 @@
+package com.example.mobile_final_project.model;
+
+public class Despesa {
+
+    private int id;
+    private double valor;
+    private String titulo, descricao, categoria;
+
+
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+}
