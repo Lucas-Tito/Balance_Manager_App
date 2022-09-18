97db20dcbc28d631b44b4f7f065fecd22d7c68ac
Lucas Tito <lucassga500@gmail.com>
2022-09-17 23:06:01 -0300

    added fragments to bottom_nav

--- app/src/main/java/com/example/mobile_final_project/MainActivity.java
+++ app/src/main/java/com/example/mobile_final_project/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.mobile_final_project;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,42 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        build_bottom_nav();
+    }
+
+    public void build_bottom_nav(){
+
+        BottomNavigationView bottom_nav_view;
+
+        bottom_nav_view = findViewById(R.id.bottomNavigationView);
+        getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout, new Home()).commit();
+        bottom_nav_view.setSelectedItemId(R.id.home);
+
+        bottom_nav_view.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment fragment = null;
+
+                switch (item.getItemId()){
+
+                    case R.id.home:
+                        fragment = new Home();
+                        break;
+
+                    case R.id.add:
+                        fragment = new Add_Screen();
+                        break;
+
+                    case R.id.more:
+                        fragment = new More_Screen();
+                        break;
+
+                }
+                getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout, fragment).commit();
+                return true;
+            }
+        });
+
     }
 }
